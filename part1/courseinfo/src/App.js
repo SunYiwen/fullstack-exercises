48eaf9ab4ac41e6f6074eddfa3efd48e45977727
evensun <evensun@tencent.com>
2023-03-08 15:47:25 +0800

    feat: 1.3 courseinfo step3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,8 +9,8 @@ const Part = (props) => (
 const Content = (props) =>
   props.contentList.map((content) => (
     <Part
-      key={content.part}
-      part={content.part}
+      key={content.name}
+      part={content.name}
       exercises={content.exercises}
     />
   ));
@@ -19,27 +19,20 @@ const Footer = (props) => <p>Number of exercises {props.total}</p>;
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
-  const contentList = [
-    {
-      part: part1,
-      exercises: exercises1,
-    },
-    {
-      part: part2,
-      exercises: exercises2,
-    },
-    {
-      part: part3,
-      exercises: exercises3,
-    },
-  ];
+  const contentList = [part1, part2, part3];
 
   return (
     <div>
