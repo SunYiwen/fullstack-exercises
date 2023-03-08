f293fd911e5cbb7da6ad58524d97c2ea3251f864
evensun <evensun@tencent.com>
2023-03-08 15:49:15 +0800

    feat: 1.4 courseinfo step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,7 +7,7 @@ const Part = (props) => (
 );
 
 const Content = (props) =>
-  props.contentList.map((content) => (
+  props.parts.map((content) => (
     <Part
       key={content.name}
       part={content.name}
@@ -19,27 +19,26 @@ const Footer = (props) => <p>Number of exercises {props.total}</p>;
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
-  const contentList = [part1, part2, part3];
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header course={course} />
-      <Content contentList={contentList} />
+      <Content parts={parts} />
       <Footer
-        total={contentList.reduce((acc, cur) => {
+        total={parts.reduce((acc, cur) => {
           return acc + cur.exercises;
         }, 0)}
       />
